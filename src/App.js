c5c0be97148c671a2170c725bcf532432867873b
Vesta-nna <anna.dominguez.anna@gmail.com>
2023-12-15 17:45:53 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.scss'
 
 function App() {
+  const [isYearly, setIsYearly] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Our Pricing</h1>
+        <div className="toggle">
+          <p>Annualy</p>
+          <label className="switch">
+            <input onClick={() => setIsYearly(!isYearly)} type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+          <p>Monthly</p>
+        </div>
       </header>
-    </div>
-  );
+      <main>
+        <section className="basic">
+          <h2>Basic</h2>
+          <h3>
+            <span>$</span>
+            {isYearly ? '199.99' : '19.99'}
+          </h3>
+          <div className="border-b">
+            <p>500 GB Storage</p>
+            <p>2 Users Allowed</p>
+            <p>Send up to 3 GB</p>
+          </div>
+          <button>Learn More</button>
+        </section>
+        <section className="professional">
+          <h2>Professional</h2>
+          <h3>
+            <span>$</span>
+            {isYearly ? '249.99' : '24.99'}
+          </h3>
+          <div className="border-b">
+            <p>1 TB Storage</p>
+            <p>5 Users Allowed</p>
+            <p>Send up to 10 GB</p>
+          </div>
+          <button>Learn More</button>
+        </section>
+        <section className="master">
+          <h2>Master</h2>
+          <h3>
+            <span>$</span>
+            {isYearly ? '399.99' : '39.99'}
+          </h3>
+          <div className="border-b">
+            <p>2 TB Storage</p>
+            <p>10 Users Allowed</p>
+            <p>Send up to 20 GB</p>
+          </div>
+          <button>Learn More</button>
+        </section>
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
